79510387094b5e6a76982d2f91ed5ca35f0883b4
Arsh Pratap <119803584+Arsh-Pratap@users.noreply.github.com>
2022-12-07 18:35:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var element = [
+  "hydogen",
+  "helium",
+  "lithium",
+  "beryllium",
+  "boron",
+  "carbon",
+  "nitrogen",
+  "oxygen",
+  "fluorine",
+  "neon",
+  "sodium",
+  "magnesium",
+  "aluminium",
+  "silicon",
+  "phosphorus",
+  "sulfur",
+  "chlorine",
+  "argon",
+  "potassium",
+  "calcium",
+];
+var atomicNumber = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+var randomNumber;
+function randomNumbers() {
+  randomNumber = Math.floor(Math.random() * 20 + 1);
+  return randomNumber;
+}
+var random = randomNumbers();
+
+var value;
+var start = true;
+
+var innerElement = element[randomNumber];
+var innerNumber = $("#atomic-number").text(atomicNumber[randomNumber]);
+$("#submit").on("click", function () {
+  value = document.getElementById("type-name").value;
+
+  if (value == element[randomNumber]) {
+    alert("Correct 😊👌 my boii ");
+    innerNumber = $("#atomic-number").text(atomicNumber[randomNumbers()]);
+      document.getElementById("type-name").value = "";
+  } else {
+    alert("Wrong 😱. Now train for another 1000 Years");
+  }
+});
+$("#again").on("click", function () {
+  innerNumber = $("#atomic-number").text(atomicNumber[randomNumbers()]);
+});
+$(document).keyup(function (e) {
+  if (e.which === 13) {
+    value = document.getElementById("type-name").value;
+
+    if (value == element[randomNumber]) {
+      alert("Correct 😊👌 my boii");
+      innerNumber = $("#atomic-number").text(atomicNumber[randomNumbers()]);
+      document.getElementById("type-name").value = "";
+    } else {
+      alert("wroWrong 😱. Now train for another 1000 Yearsng");
+    }
+  }
+});
+
